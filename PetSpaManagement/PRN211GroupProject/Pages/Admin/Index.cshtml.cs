ec2b64fe85f2f3b422dbd3dcebb90dd74674f98e
minh2003vt <mphamtran8@gmail.com>
2024-07-16 10:39:46 +0700

    validate continue

--- PetSpaManagement/PRN211GroupProject/Pages/Admin/Index.cshtml.cs
+++ PetSpaManagement/PRN211GroupProject/Pages/Admin/Index.cshtml.cs
@@ -45,13 +45,6 @@ namespace PRN211GroupProject.Pages.Admin
             Account = AccountUtilities.Instance.GetAccount(HttpContext, _accountService);
             if (Account != null)
             {
-                var roleClaim = User.FindFirst(ClaimTypes.Role);
-
-                if (roleClaim?.Value.ToString() != "Admin")
-                {
-                    errorMessage = "You do not have permission to access the Admin page.";
-                    return RedirectToPage("/Index");
-                }
                 try
                 {
                     if (_billService == null)
